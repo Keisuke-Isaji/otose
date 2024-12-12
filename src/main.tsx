ecbdbf6ce206d01c795c0033e5e629dcfd1fde3b
Keisuke-Isaji <sajiemon.999@gmail.com>
2024-12-12 15:27:24 +0900

    second commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import 'src/index.css'
+import App from 'src/App.tsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
